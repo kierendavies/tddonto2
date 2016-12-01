1055d64449fc737170f0228cc5d637428d9cbb93
Kieren Davies <kieren@kdavi.es>
2016-12-01 15:48:40 +0200

    Implement testSubClassOf

--- src/main/java/za/ac/uct/cs/tddontoi/AxiomTester.java
+++ src/main/java/za/ac/uct/cs/tddontoi/AxiomTester.java
@@ -1,16 +1,46 @@
 package za.ac.uct.cs.tddontoi;
 
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
+import static za.ac.uct.cs.tddontoi.TestResult.*;
+
 public class AxiomTester {
-    private OWLReasoner reasoner;
+    private final OWLReasoner reasoner;
+    private final OWLDataFactory dataFactory;
+    private final OWLClass nothing;
 
     public AxiomTester(OWLReasoner reasoner) {
         this.reasoner = reasoner;
+        dataFactory = reasoner.getRootOntology().getOWLOntologyManager().getOWLDataFactory();
+        nothing = dataFactory.getOWLNothing();
     }
 
-    public TestResult testSubClassOf(OWLSubClassOfAxiom axiom) {
-        return null;
+    private boolean isSatisfiable(OWLClassExpression c) {
+        return reasoner.isSatisfiable(c);
+    }
+
+    private boolean hasNamedSubClasses(OWLClassExpression c) {
+        return reasoner.getSubClasses(c, false).getFlattened().size() > 1  // always contains owl:Nothing
+            || reasoner.getEquivalentClasses(c).getEntitiesMinus(nothing).size() > 0;
+    }
+
+    private boolean hasInstances(OWLClassExpression c) {
+        return !reasoner.getInstances(c, false).isEmpty();
+    }
+
+    public TestResult testSubClassOf(OWLClassExpression c, OWLClassExpression d) {
+        OWLClassExpression cAndNotD = dataFactory.getOWLObjectIntersectionOf(c, dataFactory.getOWLObjectComplementOf(d));
+        if (hasInstances(cAndNotD)) {
+            return INCONSISTENT;
+        } else if (hasNamedSubClasses(cAndNotD)) {
+            return INCOHERENT;
+        } else if (isSatisfiable(cAndNotD)) {
+            return ABSENT;
+        } else {
+            return ENTAILED;
+        }
     }
 }
--- src/test/java/za/ac/uct/cs/tddontoi/AxiomTesterTest.java
+++ src/test/java/za/ac/uct/cs/tddontoi/AxiomTesterTest.java
@@ -1,37 +1,71 @@
 package za.ac.uct.cs.tddontoi;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.io.File;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AxiomTesterTest {
-    private OWLReasoner reasoner;
+    private static final String pizzaPath = "src/test/resources/pizza.owl";
+    private static final String pizzaPrefix = "http://www.co-ode.org/ontologies/pizza/pizza.owl#";
+
+    private OWLDataFactory dataFactory;
+    private AxiomTester axiomTester;
+
+    private OWLClass parseClass(String name) {
+        return dataFactory.getOWLClass(IRI.create(pizzaPrefix, name));
+    }
 
     @Before
     public void setUp() throws Exception {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File("src/test/resources/pizza.owl"));
-        reasoner = new Reasoner(ontology);
+        dataFactory = manager.getOWLDataFactory();
+        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(pizzaPath));
+        OWLReasoner reasoner = new Reasoner(ontology);
+
+        // Ensure preconditions
         assertTrue(reasoner.isConsistent());
         assertEquals(reasoner.getUnsatisfiableClasses().getSize(), 1);  // always includes owl:Nothing
-    }
 
-    @After
-    public void tearDown() throws Exception {
-
+        axiomTester = new AxiomTester(reasoner);
     }
 
     @Test
     public void testSubClassOf() throws Exception {
-        fail();
+        assertEquals(
+            TestResult.ENTAILED,
+            axiomTester.testSubClassOf(
+                parseClass("Margherita"),
+                parseClass("NamedPizza")
+            )
+        );
+        assertEquals(
+                TestResult.ABSENT,
+                axiomTester.testSubClassOf(
+                        parseClass("Pizza"),
+                        parseClass("NamedPizza")
+                )
+        );
+        assertEquals(
+                TestResult.INCOHERENT,
+                axiomTester.testSubClassOf(
+                        parseClass("PizzaBase"),
+                        parseClass("PizzaTopping")
+                )
+        );
+        assertEquals(
+                TestResult.INCONSISTENT,
+                axiomTester.testSubClassOf(
+                        parseClass("Country"),
+                        dataFactory.getOWLObjectComplementOf(parseClass("Country"))
+                )
+        );
     }
 }
