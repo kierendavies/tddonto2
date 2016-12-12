ddd5ecf56b1e335a79b657c587a40c4a12fae433
Kieren Davies <kieren@kdavi.es>
2016-12-12 15:28:50 +0200

    Create better Swing UI

--- src/main/java/za/ac/uct/cs/tddonto2/TestResult.java
+++ src/main/java/za/ac/uct/cs/tddonto2/TestResult.java
@@ -6,4 +6,17 @@ public enum TestResult {
     public static TestResult max(TestResult r1, TestResult r2) {
         return r1.compareTo(r2) > 0 ? r1 : r2;
     }
+
+    public String humanize() {
+        switch (this) {
+            case ENTAILED: return "Entailed";
+            case ABSENT: return "Absent";
+            case INCOHERENT: return "Incoherent";
+            case INCONSISTENT: return "Inconsistent";
+            case MISSING_ENTITY: return "Missing entity";
+            case PRE_INCOHERENT: return "Failed precondition: ontology incoherent";
+            case PRE_INCONSISTENT: return "Failed precondition: ontology inconsistent";
+        }
+        return null;
+    }
 }
--- src/main/java/za/ac/uct/cs/tddonto2/TestSuiteViewComponent.java
+++ src/main/java/za/ac/uct/cs/tddonto2/TestSuiteViewComponent.java
@@ -1,79 +1,198 @@
 package za.ac.uct.cs.tddonto2;
 
+import org.protege.editor.core.ui.util.ComponentFactory;
+import org.protege.editor.owl.model.inference.OWLReasonerManager;
+import org.protege.editor.owl.model.inference.ReasonerStatus;
+import org.protege.editor.owl.model.inference.ReasonerUtilities;
+import org.protege.editor.owl.ui.clsdescriptioneditor.ExpressionEditor;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
-import org.semanticweb.owlapi.expression.OWLEntityChecker;
 import org.semanticweb.owlapi.expression.OWLExpressionParser;
 import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
+import org.semanticweb.owlapi.io.OWLParserException;
 import org.semanticweb.owlapi.manchestersyntax.parser.ManchesterOWLSyntaxInlineAxiomParser;
 import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClassAxiom;
 import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter;
 import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class TestSuiteViewComponent extends AbstractOWLViewComponent {
 
     OWLExpressionParser<OWLAxiom> parser;
-    private JTextArea textArea;
-    private JButton evalButton;
-    private JLabel axiomLabel;
-    private JLabel resultLabel;
+    private ExpressionEditor<OWLClassAxiom> editor;
+    private JLabel editorResultLabel;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
     @Override
     protected void initialiseOWLView() throws Exception {
-        OWLEntityChecker entityChecker = new ShortFormEntityChecker(
-                new BidirectionalShortFormProviderAdapter(
-                        getOWLModelManager().getActiveOntology().getOWLOntologyManager(),
-                        getOWLModelManager().getOntologies(),
-                        new SimpleShortFormProvider()
+        parser = new ManchesterOWLSyntaxInlineAxiomParser(
+                getOWLDataFactory(),
+                new ShortFormEntityChecker(
+                        new BidirectionalShortFormProviderAdapter(
+                                getOWLModelManager().getActiveOntology().getOWLOntologyManager(),
+                                getOWLModelManager().getOntologies(),
+                                new SimpleShortFormProvider()
+                        )
                 )
         );
-        parser = new ManchesterOWLSyntaxInlineAxiomParser(getOWLDataFactory(), entityChecker);
 
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
+        setLayout(new BorderLayout(10, 10));
 
-        textArea = new JTextArea(1, 40);
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.fill = GridBagConstraints.BOTH;
-        add(textArea, constraints);
+        // Top panel with editor
 
-        evalButton = new JButton("Evaluate");
-        evalButton.addActionListener(e -> evaluateTest());
-        constraints.gridx = 1;
-        constraints.fill = GridBagConstraints.NONE;
-        add(evalButton, constraints);
+        JPanel editorPanel = new JPanel(new BorderLayout());
+        editorPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY), "New test"),
+                BorderFactory.createEmptyBorder(3, 3, 3, 3)
+        ));
+        add(editorPanel, BorderLayout.NORTH);
 
-        axiomLabel = new JLabel("No axiom");
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        add(axiomLabel, constraints);
+        editor = new ExpressionEditor<>(getOWLEditorKit(), getOWLModelManager().getOWLExpressionCheckerFactory().getClassAxiomChecker());
+        editorPanel.add(editor, BorderLayout.CENTER);
 
-        resultLabel = new JLabel("No result");
-        constraints.gridy = 2;
-        add(resultLabel, constraints);
+        JPanel editorButtonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        editorPanel.add(editorButtonsPanel, BorderLayout.SOUTH);
+
+        JButton addButton = new JButton("Add");
+        addButton.addActionListener(e -> addTest());
+        editorButtonsPanel.add(addButton);
+
+        JButton editorEvalButton = new JButton("Evaluate");
+        editorEvalButton.addActionListener(e -> evaluateEditorTest());
+        editorButtonsPanel.add(editorEvalButton);
+
+        editorResultLabel = new JLabel();
+        editorButtonsPanel.add(editorResultLabel);
+
+        // Main table
+
+        Object[] columnHeads = {"Test axiom", "Result"};
+        tableModel = new DefaultTableModel(new Object[][] {}, columnHeads);
+        table = new JTable(tableModel);
+        table.setFillsViewportHeight(true);
+        add(ComponentFactory.createScrollPane(table), BorderLayout.CENTER);
+
+        // Main buttons
+
+        JPanel mainButtonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        add(mainButtonsPanel, BorderLayout.SOUTH);
+
+        JButton evalAllButton = new JButton("Evaluate all");
+        evalAllButton.addActionListener(e -> evaluateAllTests());
+        mainButtonsPanel.add(evalAllButton);
+
+        JButton evalSelectedButton = new JButton("Evaluate selected");
+        evalSelectedButton.addActionListener(e -> evaluateSelectedTests());
+        mainButtonsPanel.add(evalSelectedButton);
+
+        JButton removeButton = new JButton("Remove selected");
+        removeButton.addActionListener(e -> removeSelectedTests());
+        mainButtonsPanel.add(removeButton);
+
+        JButton addToOntologyButton = new JButton("Add selected to ontology");
+        addToOntologyButton.addActionListener(e -> addSelectedTestsToOntology());
+        mainButtonsPanel.add(addToOntologyButton);
     }
 
     @Override
     protected void disposeOWLView() {
     }
 
-    private void evaluateTest() {
-        axiomLabel.setText("");
-        resultLabel.setText("");
+    private void addTest() {
+        tableModel.addRow(new Object[] {editor.getText(), null});
+        tableModel.fireTableDataChanged();
+    }
+
+    private void evaluateEditorTest() {
+        OWLReasonerManager reasonerManager = getOWLModelManager().getOWLReasonerManager();
+        if (reasonerManager.getReasonerStatus() != ReasonerStatus.INITIALIZED) {
+            ReasonerUtilities.warnUserIfReasonerIsNotConfigured(getOWLWorkspace(), reasonerManager);
+            return;
+        }
 
         try {
-            OWLAxiom axiom = parser.parse(textArea.getText());
-            axiomLabel.setText(axiom.toString());
+            AxiomTester axiomTester = new AxiomTester(reasonerManager.getCurrentReasoner());
 
-            AxiomTester axiomTester = new AxiomTester(getOWLModelManager().getOWLReasonerManager().getCurrentReasoner());
-            TestResult result = axiomTester.test(axiom);
-            resultLabel.setText(result.toString());
-        } catch (Exception ex) {
-            resultLabel.setText(ex.getMessage());
+            TestResult preconditionResult = axiomTester.testPreconditions();
+            if (preconditionResult != null) {
+                editorResultLabel.setText(preconditionResult.humanize());
+            } else {
+                TestResult result = axiomTester.test(parser.parse(editor.getText()));
+                editorResultLabel.setText(result.humanize());
+            }
+        } catch (OWLParserException e) {
+            editorResultLabel.setText("Syntax error or missing entity");
         }
+    }
+
+    private void evaluateAllTests() {
+        OWLReasonerManager reasonerManager = getOWLModelManager().getOWLReasonerManager();
+        if (reasonerManager.getReasonerStatus() != ReasonerStatus.INITIALIZED) {
+            ReasonerUtilities.warnUserIfReasonerIsNotConfigured(getOWLWorkspace(), reasonerManager);
+            return;
+        }
+
+        AxiomTester axiomTester = new AxiomTester(reasonerManager.getCurrentReasoner());
+
+        TestResult preconditionResult = axiomTester.testPreconditions();
+        if (preconditionResult != null) {
+            for (int row = 0; row < table.getRowCount(); ++row) {
+                table.setValueAt(preconditionResult.humanize(), row, 1);
+            }
+        } else {
+            for (int row = 0; row < table.getRowCount(); ++row) {
+                try {
+                    TestResult result = axiomTester.test(parser.parse((String) table.getValueAt(row, 0)));
+                    table.setValueAt(result.humanize(), row, 1);
+                } catch (OWLParserException e) {
+                    table.setValueAt("Syntax error or missing entity", row, 1);
+                }
+            }
+        }
+    }
+
+    private void evaluateSelectedTests() {
+        OWLReasonerManager reasonerManager = getOWLModelManager().getOWLReasonerManager();
+        if (reasonerManager.getReasonerStatus() != ReasonerStatus.INITIALIZED) {
+            ReasonerUtilities.warnUserIfReasonerIsNotConfigured(getOWLWorkspace(), reasonerManager);
+            return;
+        }
+
+        int[] selectedRows = table.getSelectedRows();
+
+        AxiomTester axiomTester = new AxiomTester(reasonerManager.getCurrentReasoner());
+
+        TestResult preconditionResult = axiomTester.testPreconditions();
+        if (preconditionResult != null) {
+            for (int row : selectedRows) {
+                table.setValueAt(preconditionResult.humanize(), row, 1);
+            }
+        } else {
+            for (int row : selectedRows) {
+                System.out.println(row);
+                try {
+                    TestResult result = axiomTester.test(parser.parse((String) table.getValueAt(row, 0)));
+                    table.setValueAt(result.humanize(), row, 1);
+                } catch (OWLParserException e) {
+                    table.setValueAt("Syntax error or missing entity", row, 1);
+                }
+            }
+        }
+    }
+
+    private void removeSelectedTests() {
+        int[] selectedRows = table.getSelectedRows();
+        for (int row : selectedRows) {
+            tableModel.removeRow(row);
+        }
+        tableModel.fireTableDataChanged();
+    }
+
+    private void addSelectedTestsToOntology() {
+
     }
 }
